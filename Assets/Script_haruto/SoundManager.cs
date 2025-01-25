791308adffd7d1729b272919a813c00740c51a4a
Munakata Haruto <zeal23420039@fsgcl.jp>
2025-01-25 12:26:01 +0900

    サウンドマネージャーを追加

---
+++ Assets/Script_haruto/SoundManager.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    [SerializeField]
+    List<AudioClip> clips;
+
+    public enum SE
+    {
+
+    }
+    
+    static SoundManager instance;
+    void Awake()
+    {
+        instance = this;
+        audioSource = GetComponent<AudioSource>();
+    }
+    public static bool CheckInstance()
+    {
+        return true;
+    }
+    public static SoundManager GetInstance()
+    {
+        CheckInstance();
+        return instance;
+    }
+
+    public void PlaySE(SE se)
+    {
+        audioSource.PlayOneShot(clips[(int)se]);
+    }
+    public void StopSE()
+    {
+        audioSource.Stop();
+    }
+
+   
+}
