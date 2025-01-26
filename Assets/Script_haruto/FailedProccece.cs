1ca827412ba6f7704489d1ae7b7af1f4c5591d7f
Munakata Haruto <zeal23420039@fsgcl.jp>
2025-01-26 15:51:13 +0900

    タイトルとクリアリザルトにBGMを追加

--- Assets/Script_haruto/FailedProccece.cs
+++ Assets/Script_haruto/FailedProccece.cs
@@ -11,7 +11,7 @@ public class FailedProccece : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        if (Input.anyKeyDown)
         {
             sceneMove.Change();
         }
